40f72741dceef55f5fa8c8b0bdb737ac79d09b44
ChingChong <cazanescurares@gmail.com>
2024-07-09 00:42:13 +0300

    ts<js

--- README.md
+++ README.md
@@ -13,4 +13,25 @@ pip install Flask Flask-Cors python-dotenv
 pip install -U Flask-SQLAlchemy
 
 pip install -r requirements.txt
+
+```
+### NODE INSTALL ###
+```
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+```
+```bash
+npm install -g npm
+
+npm create vue@latest
+cd projectname
+
+npm install
+
+npm run format
+npm run dev
 ```
--- server/app.py
+++ server/app.py
@@ -1,20 +1,43 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from sqlalchemy.orm import DeclarativeBase
 
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 # instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    phone = db.Column(db.String(20))
+    address = db.Column(db.String(200))
+    email = db.Column(db.String(100))
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+with app.app_context():
+    db.create_all()
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/add_client', methods=['POST'])
+def add_client():
+    data = request.json
+    new_client = Client(name=data['name'], phone=data['phone'], address=data['address'], email=data['email'])
+    db.session.add(new_client)
+    db.session.commit()
+    return jsonify({"message": "Client added successfully!"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
