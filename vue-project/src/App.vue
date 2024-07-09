a28eecf0b7ccd53ecac743f5c9ec902acd292728
John Doe <null@mail.com>
2024-07-09 23:27:44 +0300

    based

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -13,8 +13,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/ping">Ping</RouterLink> <!-- Add this line -->
-
       </nav>
     </div>
   </header>
