166d1cb812f46dd9b910b7c86db194a4bcea0e85
John Doe <null@mail.com>
2024-07-26 18:29:52 +0300

    vue cleanup app main ping

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,32 +1,14 @@
-// import './assets/main.css'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import 'vuetify/dist/vuetify.min.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from '../plugins/vuetify'
+import vuetify from './plugins/vuetify'
 import router from './router'
 import 'vuetify/styles' 
 
-
-
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-
-// Load environment variable
 const apiUrl = import.meta.env.VITE_API_URL;
 
 const app = createApp(App)
 app.config.globalProperties.$apiUrl = apiUrl;
 
-
 app.use(router)
-app.use(VueAxios, axios)
-// app.use(bootstrap)
 app.use(vuetify)
-
 app.mount('#app')
-
-// Vue.config.devtools = true;
